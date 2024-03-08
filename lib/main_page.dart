d271678049880fd241b4bebfab6146177a69e60f
Januda Bethmin <desilvabethmin@gmail.com>
2024-03-08 15:39:41 +0530

    added icons

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,8 +10,48 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    // You need to warp the container using a Scaffold then from the SafeArea.
+    return Scaffold(
+      body: SafeArea (
+        child : Container(
+          color: Colors.white,
+          child: const Column(
+            children: [
+              Row(
+                children: [
+                  // Warpped by a padding widget
+                  Padding( 
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        // You can use FontAwesome icons, Material icons etc. here
+                        Icon(Icons.male, size: 150),
+                        Text("Male"),
+                      ],
+                    ),
+                  ),
+                  Spacer(), // Creates a space between widgets while getting the maximum space as it can get.
+                  // Warpped by a padding widget
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.female, size: 150),
+                        Text("Female"),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
