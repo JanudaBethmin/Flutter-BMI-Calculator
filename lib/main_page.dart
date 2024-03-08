ee25abc79783dedd0ad5b729cd8b79019e445906
Januda Bethmin <desilvabethmin@gmail.com>
2024-03-08 16:15:59 +0530

    made the basic structure of the UI

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -45,7 +45,73 @@ class _MainPageState extends State<MainPage> {
               ),
               Row(
                 children: [
-                  
+                  // Warpped by a padding widget
+                  Padding( 
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text("Height"),
+                        // Added styles for the Text using style: TextStyle()
+                        Text("176", 
+                        style: TextStyle(
+                          color: Color(0xFFFF8888), 
+                          fontSize: 50, fontWeight: 
+                          FontWeight.bold)
+                        ),
+                        Row(
+                          children: [
+                            FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.remove, size: 40)
+                            ),
+                            SizedBox(width: 20), // You can't add a spacer here. Flutter can't identify how mush space it as.
+                            // So we use a Sized box. It is a constant space that has a fixed size.
+                            FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.add, size: 40)
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Spacer(), // Creates a space between widgets while getting the maximum space as it can get.
+                  // Warpped by a padding widget
+                  Padding( 
+                    padding: EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Text("Weight"),
+                        // Added styles for the Text using style: TextStyle()
+                        Text("70", 
+                        style: TextStyle(
+                          color: Color(0xFFFF8888), 
+                          fontSize: 50, fontWeight: 
+                          FontWeight.bold)
+                        ),
+                        Row(
+                          children: [
+                            FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.remove, size: 40)
+                            ),
+                            SizedBox(width: 20), // You can't add a spacer here. Flutter can't identify how mush space it as.
+                            // So we use a Sized box. It is a constant space that has a fixed size.
+                            FloatingActionButton(
+                              onPressed: null,
+                              child: Icon(Icons.add, size: 40)
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text("BMI"),
+                  Text("22.22"),
                 ],
               ),
             ],
